9e65c7ab6bc6f01a93a683111ccb4fc0d0ee8152
fwcd <fwcdmail@gmail.com>
2023-12-01 00:25:22 +0000

    WIP: Source macOS buildenv wrappers

--- tools/macos_arm64-cross-release_buildenv.sh
+++ tools/macos_arm64-cross-release_buildenv.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-tools_path=$(dirname "$0")
-BUILDENV_ARM64=TRUE BUILDENV_RELEASE=TRUE "${tools_path}/macos_buildenv.sh" "$@"
+
+# shellcheck disable=SC2091
+if [ -z "${GITHUB_ENV}" ] && ! $(return 0 2>/dev/null); then
+  echo "This script must be run by sourcing it:"
+  echo "source $0 $*"
+  exit 1
+fi
+
+# Find the parent directory in a way compatible with bash and zsh
+tools_path="${BASH_SOURCE%/*}"
+[ -z "$tools_path" ] && tools_path="$(dirname "$0")"
+
+export BUILDENV_ARM64=TRUE
+export BUILDENV_RELEASE=TRUE
+
+# shellcheck disable=SC1091
+source "${tools_path}/macos_buildenv.sh" "$@"
