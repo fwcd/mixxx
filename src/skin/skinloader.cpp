275f4d0c6a92d382beab6b098e2e4c9ad360bfc4
fwcd <fwcd@fwcd.dev>
2024-01-27 23:57:36 +0100

    Bundle a modified LateNight skin optimized for small displays
    Credit goes to to @dennisdebel for the awesome work:
    
        https://github.com/dennisdebel/pi_dj

--- src/skin/skinloader.cpp
+++ src/skin/skinloader.cpp
@@ -125,7 +125,11 @@ SkinPointer SkinLoader::getConfiguredSkin() const {
 }
 
 QString SkinLoader::getDefaultSkinName() const {
+#ifdef Q_OS_IOS
+    return "LateNightMini";
+#else
     return "LateNight";
+#endif
 }
 
 QWidget* SkinLoader::loadConfiguredSkin(QWidget* pParent,
