34c1efcbaa35d2233ba4b04f8b4a55ed17846e71
fwcd <fwcd@fwcd.dev>
2024-01-29 20:41:04 +0100

    SoundManager: Set up an AVAudioSession on iOS

---
+++ src/soundio/soundmanagerios.mm
@@ -0,0 +1,26 @@
+#include <QDebug>
+#include <QtGlobal>
+
+#import <AVFAudio/AVFAudio.h>
+
+namespace mixxx {
+
+void initializeAVAudioSession() {
+    AVAudioSession* session = AVAudioSession.sharedInstance;
+    AVAudioSessionCategory category = AVAudioSessionCategoryPlayAndRecord;
+    AVAudioSessionMode mode = AVAudioSessionModeMeasurement;
+    AVAudioSessionCategoryOptions options = AVAudioSessionCategoryOptionMixWithOthers;
+    
+    NSError* error = nil;
+    [session setCategory:category mode:mode options:options error:&error];
+    if (error != nil) {
+        qWarning() << "Could not initialize AVAudioSession:" << error.localizedDescription;
+    }
+    
+    [session setActive:true error:&error];
+    if (error != nil) {
+        qWarning() << "Could not activate AVAudioSession:" << error.localizedDescription;
+    }
+}
+
+}; // namespace mixxx
