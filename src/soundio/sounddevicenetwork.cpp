5fe74e2adf4eca478f44c0875e231117c27eb376
fwcd <fwcd@fwcd.dev>
2024-03-08 04:02:35 +0100

    SoundDeviceNetwork: Remove debug assert for denormals-to-zero
    Denormals to zero are currently unavailable as it seems, so we'll have
    to depromote this assert to a warning for now and look for another
    way to implement this for WASM.

--- src/soundio/sounddevicenetwork.cpp
+++ src/soundio/sounddevicenetwork.cpp
@@ -485,7 +485,11 @@ void SoundDeviceNetwork::callbackProcessClkRef() {
         // verify if flush to zero or denormals to zero works
         // test passes if one of the two flag is set.
         volatile double doubleMin = DBL_MIN; // the smallest normalized double
+#ifdef __EMSCRIPTEN__
+        if (doubleMin / 2 != 0.0) {
+#else
         VERIFY_OR_DEBUG_ASSERT(doubleMin / 2 == 0.0) {
+#endif
             qWarning() << "Network Sound: Denormals to zero mode is not working. "
                           "EQs and effects may suffer high CPU load";
         }
